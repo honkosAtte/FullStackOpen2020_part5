f4a5aa3b4b3368fc983e202f8ba86b6ba008bfb0
Atte Honkonen <atte.honkonen@gmail.com>
2021-01-24 22:21:43 +0200

    9.16

--- part_9_patientor/src/services/patientService.ts
+++ part_9_patientor/src/services/patientService.ts
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 
 import patientEntries from '../data/patientsdata';
-import { Patient, PatientWithoutSSN, NewPatient } from '../types';
+import { Patient, PublicPatient, NewPatient } from '../types';
 
 let patients: Patient[] = patientEntries;
 
@@ -9,8 +9,8 @@ export const getEntries = (): Patient[] => {
     return patients;
 };
 
-export const getEntriesWithNoSSN = (): PatientWithoutSSN[] => {
-    return patients.map(({id, name, dateOfBirth, gender, occupation}) => ({id, name, dateOfBirth, gender, occupation}));
+export const getEntriesWithNoSSN = (): PublicPatient[] => {
+    return patients.map(({id, name, dateOfBirth, gender, occupation}) => ({id, name, dateOfBirth, gender, occupation, entries: []}));
 };
 
 
--- part_9_patientor/src/types.ts
+++ part_9_patientor/src/types.ts
@@ -4,14 +4,14 @@ export type Diagnose = {
     latin?: string;
 };
 
-export type Patient = {
-    id: string,
-    name: string,
-    dateOfBirth: string,
-    ssn: string,
-    gender: Gender,
-    occupation: string
-};
+// export type Patient = {
+//     id: string,
+//     name: string,
+//     dateOfBirth: string,
+//     ssn: string,
+//     gender: Gender,
+//     occupation: string
+// };
 
 export enum Gender {
     MALE = 'male', 
@@ -23,3 +23,17 @@ export type PatientWithoutSSN = Omit<Patient, 'ssn'>;
 
 export type NewPatient = Omit<Patient, 'id'>;
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Entry {
+}
+
+export interface Patient {
+  id: string;
+  name: string;
+  ssn: string;
+  occupation: string;
+  gender: Gender;
+  dateOfBirth: string;
+  entries: Entry[]}
+
+export type PublicPatient = Omit<Patient, 'ssn' | 'entries' >;
